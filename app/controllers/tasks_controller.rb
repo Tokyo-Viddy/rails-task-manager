17c01f549d5ae4abee5d0d7ea704385b6f9e2f0a
David Gregorato <HydroSnake@gmail.com>
2021-08-10 15:07:14 +0900

    Finished all the user stories, need to refactor the code.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+# Controller for the tasks
+class TasksController < ApplicationController
+  before_action :show_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+
+    # refactor later
+    if @task.completed == false
+      return @task_completed = '👎🏻'
+    else
+      return @task_completed = '👍🏻'
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    # raise
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def show_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
